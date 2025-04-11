2fbea25bd673a0ae1c75cf1d53ff4eb83379ba38
jm@habit-streak.app <support@habit-streak.app>
2025-04-12 00:13:29 +0200

    copy page index

---
+++ custom/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useBreadcrumbStore } from '~/stores/breadcrumb';
+
+const storeBreadcrumb = useBreadcrumbStore();
+storeBreadcrumb.clear();
+</script>
+
+<template>
+    <section class="page">
+        <div class="mx-auto max-w-6xl grid grid-cols-6 gap-3">
+            <HomepageHero />
+            <CatalogProductGrid identifier="helme" />
+        </div>
+    </section>
+</template>
