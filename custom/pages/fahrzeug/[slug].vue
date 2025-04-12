b2e59af513465e431bcfb1b04c86f661c0f6bc14
jm@habit-streak.app <support@habit-streak.app>
2025-04-13 00:51:13 +0200

    add fahrzeug view page

---
+++ custom/pages/fahrzeug/[slug].vue
@@ -0,0 +1,18 @@
+<template>
+    Hallo Welt
+</template>
+
+<script setup>
+import PocketBase from 'pocketbase';
+import { usePocketbaseStore } from '~/stores/pocketbase';
+import { useRoute, useRouter } from 'vue-router';
+
+const store = usePocketbaseStore();
+const { url } = storeToRefs(store);
+const pb = new PocketBase(url.value);
+
+const products = ref([]);
+const query = ref('');
+const route = useRoute();
+const router = useRouter();
+</script>
