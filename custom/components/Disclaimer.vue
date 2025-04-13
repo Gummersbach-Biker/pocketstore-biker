956e505beac6ad1cce9b81633356890b2c1fbb3d
jm@habit-streak.app <support@habit-streak.app>
2025-04-13 21:50:04 +0200

    style disclaimer

--- custom/components/Disclaimer.vue
+++ custom/components/Disclaimer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="alert bg-transparent text-white font-bold">
+    <div class="glass text-red-600 font-bold mx-3 flex px-3 py-3 justify-center">
         <Fa :icon="faCircleInfo" />
-        <p class="text-sm">
+        <p class="text-sm mx-3">
             Dieser Shop ist ein Demo Shop für PocketStore.io, ist also nur zum Testen.
         </p>
         <Fa :icon="faCircleInfo" />
