b59f6008f34656b751dfd1999c237888ce612f4d
jonathan-martz <github@jmartz.de>
2025-09-07 21:02:23 +0200

    work on initial theme for biker

---
+++ custom/components/Header.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="header bg-[#113F67] mb-3 mx-auto max-w-6xl text-white px-3 py-3">
+    <section class="flex justify-between">
+      <a href="/" class="logo flex items-center">
+        <img src="/biker-logo.svg" class="h-16" alt="">
+        <span class="ml-3 text-white font-bold text-lg">Gummersbach<br>Biker.shop</span>
+      </a>
+      <section class="navigation block xs:flex items-end flex-col">
+        <button class="btn btn-sm btn-primary btn-block mb-3">Fahrzeug mieten</button>
+        <ul class="flex space-x-5 min-w-md">
+          <li>
+            <FontAwesomeIcon :icon="['fas', 'helmet-safety']"/>
+            <a href="/bikes" class="ml-3">Helme</a>
+          </li>
+          <li>
+            <FontAwesomeIcon :icon="['fas', 'user-secret']"/>
+            <a href="/bikes" class="ml-3">Kleidung</a>
+          </li>
+          <li>
+            <FontAwesomeIcon :icon="['fas', 'motorcycle']"/>
+            <a href="/bikes" class="ml-3">Bikes</a>
+          </li>
+          <li>
+            <FontAwesomeIcon :icon="['fas', 'motorcycle']"/>
+            <a href="/bikes" class="ml-3">Bikes</a>
+          </li>
+          <li>
+            <FontAwesomeIcon :icon="['fas', 'motorcycle']"/>
+            <a href="/bikes" class="ml-3">Bikes</a>
+          </li>
+        </ul>
+      </section>
+    </section>
+  </section>
+</template>
+
+<script setup lang="ts">
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+</script>
---
+++ custom/layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="page bg-cover min-h-screen" data-theme="light">
+        <header>
+            <Header />
+        </header>
+        <main class="mx-auto max-w-6xl min-h-screen px-3 2xl:px-0">
+            <Messages />
+            <Breadcrumb />
+            <CookieBanner />
+            <BackToTop />
+            <slot />
+        </main>
+        <footer>
+            <Footer />
+        </footer>
+    </section>
+</template>
+<script setup lang="ts">
+import "../main.css";
+import Footer from "~/components/Footer.vue";
+import Header from "~/components/Header.vue";
+</script>
+
+<style scoped>
+.page {
+  background-image: url("/harley-davidson-zGzXsJUBQfs-unsplash.jpg");
+}
+</style>
+
