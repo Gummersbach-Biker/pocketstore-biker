8ec3c9a06bfb8acd013bbb2b72b25aa9cea3618d
jonathan-martz <github@jmartz.de>
2025-09-07 21:21:02 +0200

    update manifest for biker and header

--- custom/components/Header.vue
+++ custom/components/Header.vue
@@ -5,33 +5,43 @@
   <section class="header bg-[#113F67] mb-3 mx-auto max-w-6xl text-white px-3 py-3">
     <section class="flex justify-between">
       <a href="/" class="logo flex items-center">
-        <img src="/biker-logo.svg" class="h-16" alt="">
+        <img src="/biker-logo.svg" class="h-12" alt="">
         <span class="ml-3 text-white font-bold text-lg">Gummersbach<br>Biker.shop</span>
       </a>
-      <section class="navigation block xs:flex items-end flex-col">
-        <button class="btn btn-sm btn-primary btn-block mb-3">Fahrzeug mieten</button>
+      <section class="navigation flex items-end md:flex-col">
         <ul class="flex space-x-5 min-w-md">
           <li>
-            <FontAwesomeIcon :icon="['fas', 'helmet-safety']"/>
-            <a href="/bikes" class="ml-3">Helme</a>
+            <a href="/de/category/helm.html">
+              <FontAwesomeIcon :icon="['fas', 'helmet-safety']"/>
+              <span class="ml-3">Helme</span>
+            </a>
           </li>
           <li>
-            <FontAwesomeIcon :icon="['fas', 'user-secret']"/>
-            <a href="/bikes" class="ml-3">Kleidung</a>
+            <a href="/de/category/kleidung.html">
+              <FontAwesomeIcon :icon="['fas', 'user-secret']"/>
+              <span class="ml-3">Kleidung</span>
+            </a>
           </li>
           <li>
-            <FontAwesomeIcon :icon="['fas', 'motorcycle']"/>
-            <a href="/bikes" class="ml-3">Bikes</a>
+            <a href="/de/category/motorräder.html">
+              <FontAwesomeIcon :icon="['fas', 'motorcycle']"/>
+              <span class="ml-3">Bikes</span>
+            </a>
           </li>
           <li>
-            <FontAwesomeIcon :icon="['fas', 'motorcycle']"/>
-            <a href="/bikes" class="ml-3">Bikes</a>
+            <a href="/de/wunschzettel">
+              <FontAwesomeIcon :icon="['fas', 'heart']"/>
+              <span class="ml-3">Wunschzettel</span>
+            </a>
           </li>
           <li>
-            <FontAwesomeIcon :icon="['fas', 'motorcycle']"/>
-            <a href="/bikes" class="ml-3">Bikes</a>
+            <a href="/de/checkout">
+              <FontAwesomeIcon :icon="['fas', 'shopping-cart']"/>
+              <span class="ml-3">Warenkorb</span>
+            </a>
           </li>
         </ul>
+        <a href="/de/mieten" class="btn btn-sm btn-primary btn-block mt-3">Fahrzeug mieten</a>
       </section>
     </section>
   </section>
@@ -39,4 +49,13 @@
 
 <script setup lang="ts">
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+useHead({
+  link: [{
+    rel: 'icon',
+    type: 'image/svg+xml',
+    href: '/biker-logo.svg'
+  }]
+})
+
 </script>
