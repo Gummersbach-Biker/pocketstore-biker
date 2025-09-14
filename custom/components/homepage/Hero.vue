f3e5a1caf4f4bb459671e816e5f9b34d177fe48a
jonathan-martz <github@jmartz.de>
2025-09-14 20:38:56 +0200

    work on header

--- custom/components/homepage/Hero.vue
+++ custom/components/homepage/Hero.vue
@@ -7,7 +7,7 @@
         <div>
           <h1 class="text-2xl font-bold">Willkommen in unserem Online-Shop für Motoräder und Kleidung</h1>
           <p data-testid="p-hero" class="py-6">
-            Gerne empfangen wir euch auch in unserem Laden in Buxthude :D -
+            Gerne empfangen wir euch auch in unserem Laden in Buxthude :D <br>
             Persönlich und Kompenent auch vor Ort. Folgt uns auch auf <a href="/">Instagram</a>.
           </p>
           <section class="actions space-x-3">
